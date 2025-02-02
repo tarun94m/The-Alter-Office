288e9b8c86c1025670a152d38c572bd39af2ba04
Tarun M <sachitmanjunath98@gmail.com>
2025-02-02 23:45:04 +0530

    Initial commit

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { , validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'the-alter-office-main',
+  location: 'us-central1'
+};
+
